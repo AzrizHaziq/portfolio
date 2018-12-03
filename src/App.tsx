7591e0bfe59d9ad38e2e6797c7aabd1ca0ce31d1
AzrizHaziq <azrizhaziq@gmail.com>
2018-12-03 23:12:48 +0800

    * done avatar * done social apps

--- src/App.tsx
+++ src/App.tsx
@@ -1,28 +1,26 @@
 import React, { Component } from 'react';
-import logo from './logo.svg';
-import './styles/App.scss';
+import { Avatar } from './components/avatar/avatar'
+import { Navbar } from './components/navbar/navbar'
+import { SocialLinks } from './components/social-links/social-links'
 
 class App extends Component {
   render() {
-    return (
-      <div className="App">
-        <header className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <p>
-            Edit <code>src/App.tsx</code> and save to reload.
-          </p>
-          <a
-            className="App-link"
-            href="https://reactjs.org"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            Learn React
-          </a>
-        </header>
+    return <>
+      <Navbar/>
+      <div className="container">
+        <div className="p-4"></div>
+        <div className="d-flex justify-content-center align-items-center">
+          <Avatar/>
+        </div>
+        <div className="p-4"></div>
+        <div className="p-3"></div>
+        <div className="d-flex justify-content-center align-items-center">
+          <SocialLinks/>
+        </div>
       </div>
-    );
+    </>;
   }
 }
 
 export default App;
+
---
+++ src/components/navbar/navbar.tsx
@@ -0,0 +1,20 @@
+import React from 'react'
+
+export const Navbar = () => (
+    <nav className="navbar navbar-expand-sm bg-warning navbar-light justify-content-center border-bottom shadow-sm">
+      <ul className="navbar-nav">
+        <li className="nav-item">
+          <a className="nav-link" href="#">About Me</a>
+        </li>
+        <li className="nav-item">
+          <a className="nav-link" href="#">Projects</a>
+        </li>
+        <li className="nav-item">
+          <a className="nav-link" href="#">Hobby</a>
+        </li>
+        <li className="nav-item">
+          <a className="nav-link" href="#">Contact Me</a>
+        </li>
+      </ul>
+    </nav>
+)
