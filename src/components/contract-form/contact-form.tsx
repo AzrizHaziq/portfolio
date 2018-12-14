b0da73e360ab471d3f1e70a07adfe1dedf91e1d5
AzrizHaziq <azrizhaziq@gmail.com>
2018-12-14 10:15:27 +0800

    add more padding to contact form inputs

--- src/components/contract-form/contact-form.tsx
+++ src/components/contract-form/contact-form.tsx
@@ -17,21 +17,21 @@ class ContactForm extends Component {
         <div className="row justify-content-md-center">
           <div className="col-md-8">
             <form>
-              <div className="form-group">
+              <div className="form-group mb-4">
                 <label htmlFor="name">Name</label>
-                <input type="text" className="form-control" id="name" placeholder="John Smith"/>
+                <input type="text" className="form-control p-4" id="name" placeholder="John Smith"/>
               </div>
-              <div className="form-group">
+              <div className="form-group mb-4">
                 <label htmlFor="email">Email address</label>
-                <input type="email" className="form-control" id="email" aria-describedby="emailHelp"
+                <input type="email" className="form-control p-4" id="email" aria-describedby="emailHelp"
                        placeholder="johhsmith@example.com"/>
                 <small id="emailHelp" className="form-text text-muted">
                   I'll never share your email with anyone else. 😁
                 </small>
               </div>
-              <div className="form-group">
+              <div className="form-group mb-4">
                 <label htmlFor="message">Message</label>
-                <textarea className="form-control" id="message" placeholder="write an essay for me"></textarea>
+                <textarea className="form-control p-4" id="message" placeholder="write an essay for me"></textarea>
               </div>
               <button type="submit" className="btn btn-warning">Submit</button>
             </form>
