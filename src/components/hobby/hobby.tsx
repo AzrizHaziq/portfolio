eab22716d243939a3c64b17890d4367101e1ed71
AzrizHaziq <azrizhaziq@gmail.com>
2018-12-14 09:20:47 +0800

    done hobby

--- src/components/hobby/hobby.tsx
+++ src/components/hobby/hobby.tsx
@@ -1,4 +1,26 @@
 import React from 'react'
+import uuid from 'uuid/v1';
+import hobby from './hobby-list.json';
+
+const blog = Object.keys(hobby).map(blog => {
+
+  // @ts-ignore
+  const list = hobby[blog].map((post: Post, i) => (
+      <li className="list-group-item" key={uuid()}>
+        <a href={post.link}
+           target='_blank'
+           rel='noreferrer noopener'
+        >{post.title}</a>
+      </li>
+  ));
+
+  return <div key={uuid()}>
+    <h5>{blog}</h5>
+    <ul className="list-group mb-4">
+      {list}
+    </ul>
+  </div>
+})
 
 export const Hobby = () => {
   return <>
@@ -7,11 +29,33 @@ export const Hobby = () => {
     </a>
     <div className="container">
       <div className="row justify-content-md-center">
-        <div className="col" style={{ fontSize: '1.5em' }}>
-          hobby
+        <div className="col-xs-12 col-md-6" style={{ fontSize: '1.5em' }}>
+          <h4>Hobby</h4>
+
+          <ul className="list-group mb-4">
+            <li className="list-group-item">Reading blog</li>
+            <li className="list-group-item">Playing Games</li>
+            <li className="list-group-item">Read some blog</li>
+          </ul>
+        </div>
+        <div className="col-xs-12 col-md-6" style={{ fontSize: '1.5em' }}>
+          <h4>Personal Blog</h4>
+          <p>In my free time, I write some blog to share my knowledge. Any feedback are most welcome 😁.</p>
+          {blog}
         </div>
       </div>
     </div>
   </>
+}
+
+
+interface Hobby {
+  [k: string]: Post[]
+}
+
+interface Post {
+  title: string;
+  created_at: string;
+  link: string;
 }
 
--- src/components/project/project.tsx
+++ src/components/project/project.tsx
@@ -3,7 +3,7 @@ import React from 'react'
 export const Project = () => {
   return <>
     <a id='project'>
-      <h1 className="text-center text-uppercase mb-4">Project</h1>
+      <h1 className="text-center text-uppercase mb-4 text-warning">Project</h1>
     </a>
     <div className="container">
       <div className="row justify-content-md-center">
