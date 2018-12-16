bc72f9ef06e3e5574b52290ae3d6ad0c04f0035f
AzrizHaziq <azrizhaziq@gmail.com>
2018-12-16 09:36:54 +0800

    fix project view

--- src/components/project/project.tsx
+++ src/components/project/project.tsx
@@ -20,14 +20,14 @@ export const Project = () => {
 const projectList = projects.map(({ name, projectTime, description, details }) => (
     <div className="col-xs-12 col-md-6 col-lg-4" key={uuid()}>
       <div className="border shadow p-4 rounded mb-4 bg-dark">
-        <div className='d-flex align-items-center justify-content-between mb-4'>
-          <h3 className="d-inline-block mr-2 mb-0">{name}</h3>
-          <h4 className="d-inline-block text-white-50 mb-0">{projectTime}</h4>
+        <div className='d-flex flex-column flex-md-row align-items-start justify-content-start align-items-md-center justify-content-md-between mb-4'>
+          <h3 className="mr-2 mb-0">{name}</h3>
+          <small className="text-white-50 mb-0">{projectTime}</small>
         </div>
         <p className="">{description}</p>
 
         <hr className='mb-4'/>
-        <h4>HighLights</h4>
+        <h5>HighLights</h5>
         <ul className="pl-4">
           {details.map((detail, i) =>
               <li className="" key={i}>{detail}</li>
