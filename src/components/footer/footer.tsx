a3cd261f09e5e98ab8614fc8d08d94eb1ea1309a
AzrizHaziq <azrizhaziq@gmail.com>
2018-12-16 09:20:39 +0800

    relocate navbar and footer to index.html

--- src/components/footer/footer.tsx
+++
@@ -1,9 +0,0 @@
-import React from 'react'
-
-export const Footer = () => {
-  return (
-      <div className='bg-warning p-4 d-flex justify-content-center'>
-        Copyright © Azriz Haziq 2018
-      </div>
-  )
-}
