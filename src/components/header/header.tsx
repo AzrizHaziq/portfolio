c0490a860e1696c928abbee2d61b98192941b806
azrizhaziq <azrizhaziqoffice@gmail.com>
2018-12-24 16:14:43 +0800

    add smooth scroll when click headers

--- src/components/header/header.tsx
+++ src/components/header/header.tsx
@@ -1,35 +1,45 @@
-import React from 'react'
+import uuid from 'uuid/v1'
+import React, { Component } from 'react'
 
-export const Header = () => {
-  return <header>
+const headersLink = [
+  {
+    'name': 'About Me',
+    'href': 'about-me',
+  }, {
+    'name': 'Projects',
+    'href': 'projects',
+  }, {
+    'name': 'Hobbies',
+    'href': 'hobbies',
+  }, {
+    'name': 'Skills',
+    'href': 'skills',
+  }, {
+    'name': 'Contact Me',
+    'href': 'contact-me',
+  },
+]
+
+export class Header extends Component {
+  handleClick = (event, href: string) => {
+    const node = document.getElementById(href)
+    node.scrollIntoView({ behavior: 'smooth', block: 'center', inline: 'nearest' })
+    event.preventDefault()
+  }
+
+  render() {
+    return <header>
       <nav className="navbar sticky-top navbar-expand bg-warning d-flex justify-content-center border-bottom shadow-sm">
         <ul className="navbar-nav flex-all-center">
-          <li className="nav-item">
-            <a className="nav-link text-center" href="#about-me">
-              About Me
-            </a>
-          </li>
-          <li className="nav-item">
-            <a className="nav-link text-center" href="#projects">
-              Projects
-            </a>
-          </li>
-          <li className="nav-item">
-            <a className="nav-link text-center" href="#hobbies">
-              Hobbies
-            </a>
-          </li>
-          <li className="nav-item">
-            <a className="nav-link text-center" href="#skills">
-              Skills
-            </a>
-          </li>
-          <li className="nav-item">
-            <a className="nav-link text-center" href="#contact-me">
-              Contact Me
-            </a>
-          </li>
+          {headersLink.map(({ name, href }) => (
+            <li className="nav-item" key={uuid()}>
+              <a className="nav-link text-center" href={'#' + href} onClick={(event) => this.handleClick(event, href)}>
+                {name}
+              </a>
+            </li>
+          ))}
         </ul>
       </nav>
     </header>
+  }
 }
