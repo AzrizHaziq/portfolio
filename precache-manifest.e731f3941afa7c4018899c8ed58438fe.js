a6356f498fa806d5a7b577121494dfe9b8a0d6b7
Deployment Bot (from Travis CI) <deploy@travis-ci.org>
2019-01-11 07:58:28 +0000

    Deploy AzrizHaziq/portfolio to github.com/AzrizHaziq/portfolio.git:gh-pages

---
+++ precache-manifest.e731f3941afa7c4018899c8ed58438fe.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = [
+  {
+    "revision": "c480b764e9e3e4f59e05",
+    "url": "/portfolio/static/css/main.db45d20e.chunk.css"
+  },
+  {
+    "revision": "c480b764e9e3e4f59e05",
+    "url": "/portfolio/static/js/main.c480b764.chunk.js"
+  },
+  {
+    "revision": "f50783d2532f0c5cc96a",
+    "url": "/portfolio/static/js/1.f50783d2.chunk.js"
+  },
+  {
+    "revision": "ee2a100325fd284f0097",
+    "url": "/portfolio/static/js/runtime~main.ee2a1003.js"
+  },
+  {
+    "revision": "006fd36cb2f2c85b9bb8a56f5af830da",
+    "url": "/portfolio/index.html"
+  }
+];
