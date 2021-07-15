1001f28c0b2d4e18db64b6c188f0aea99993b3eb
azrizhaziq <azrizhaziq@gmail.com>
2021-07-15 16:45:31 +0800

    Move personal data context to _app.tsx

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,17 +1,50 @@
 import '../styles/globals.scss'
 
+import App from 'next/app'
+import { v4 as uuid } from 'uuid'
 import SEO from '../next-seo.config'
 import { DefaultSeo } from 'next-seo'
+import data from '../personal-data.json'
 import type { AppProps } from 'next/app'
 import { ThemeProvider } from 'next-themes'
+import { PersonalData, PersonalDataProvider } from '@helpers'
 
-export default function MyApp({ Component, pageProps }: AppProps) {
+export default function MyApp({ Component, pageProps, PersonalData }: AppProps & { PersonalData: PersonalData }) {
   return (
     <>
-      <DefaultSeo {...SEO} />
+      <DefaultSeo {...SEO} />{' '}
       <ThemeProvider attribute='class'>
-        <Component {...pageProps} />
+        <PersonalDataProvider value={PersonalData}>
+          <Component {...pageProps} />
+        </PersonalDataProvider>
       </ThemeProvider>
     </>
   )
+}
+
+MyApp.getInitialProps = async (appContext: any) => {
+  const appProps = await App.getInitialProps(appContext)
+
+  const colors = [
+    'bg-yellow-100 text-yellow-600',
+    'bg-red-200 text-red-600',
+    'bg-blue-200 text-blue-600',
+    'bg-green-200 text-green-600',
+    'bg-white text-black border-1 border-black',
+  ]
+
+  function random(min: number, max: number): number {
+    return Math.floor(Math.random() * (max - min) + min)
+  }
+
+  const shuffle = (arr: any[]): any[] =>
+    [...arr].reduceRight((res, _, __, s) => (res.push(s.splice(0 | (Math.random() * s.length), 1)[0]), res), [])
+
+  const mergedSkills = shuffle(data.skills).map(item => ({
+    skill: item,
+    id: uuid(),
+    color: colors[random(0, colors.length)],
+  }))
+
+  return { ...appProps, PersonalData: { ...data, skills: mergedSkills } }
 }
--- pages/index.tsx
+++ pages/index.tsx
@@ -1,39 +1,11 @@
-import { v4 as uuid } from 'uuid'
-import data from 'personal-data.json'
 import { useEvent } from 'react-use'
-import { PersonalDataProvider } from '@helpers'
 import { useCallback, useRef, useState } from 'react'
-import { GetStaticProps, InferGetStaticPropsType } from 'next'
 import { HeroSection, IconBox, Nav, Skills } from '@components'
-
-export const getStaticProps: GetStaticProps = async () => {
-  const colors = [
-    'bg-yellow-100 text-yellow-600',
-    'bg-red-200 text-red-600',
-    'bg-blue-200 text-blue-600',
-    'bg-green-200 text-green-600',
-    'bg-white text-black border-1 border-black',
-  ]
-
-  function random(min: number, max: number): number {
-    return Math.floor(Math.random() * (max - min) + min)
-  }
-
-  const shuffle = (arr: any[]): any[] =>
-    [...arr].reduceRight((res, _, __, s) => (res.push(s.splice(0 | (Math.random() * s.length), 1)[0]), res), [])
-
-  const mergedSkills = shuffle(data.skills).map(item => ({
-    skill: item,
-    id: uuid(),
-    color: colors[random(0, colors.length)],
-  }))
-
-  return { props: { data: { ...data, skills: mergedSkills } } }
-}
 
 const navHeader = '88px'
 const arrowHeight = '6vh'
-export default function Home({ data }: InferGetStaticPropsType<typeof getStaticProps>) {
+
+export default function Home() {
   const scrollRef = useRef<HTMLDivElement>(null)
   const [showArrow, setShowArrow] = useState<boolean>(true)
 
@@ -46,7 +18,7 @@ export default function Home({ data }: InferGetStaticPropsType<typeof getStaticP
   useEvent('scroll', onScroll)
 
   return (
-    <PersonalDataProvider value={data}>
+    <>
       <Nav />
       <main className='max-w-7xl mx-auto'>
         <div
@@ -66,6 +38,6 @@ export default function Home({ data }: InferGetStaticPropsType<typeof getStaticP
         </div>
       </main>
       <div className='mb-[400px]' />
-    </PersonalDataProvider>
+    </>
   )
 }
