60531d54175a5b2469b5b5389087b0465c45dc80
azrizhaziq <azrizhaziq@gmail.com>
2021-07-20 09:16:32 +0800

    done 404 page

--- components/useToggle.tsx
+++ components/useToggle.tsx
@@ -19,15 +19,15 @@ export function UseToggle() {
   }
 
   return (
-    <div onClick={toggle} className='cursor-pointer'>
+    <button aria-label={`Toggle to ${isDark ? 'Light' : 'Dark'} Mode`} onClick={toggle} className='cursor-pointer'>
       <span className='sr-only'>{isDark ? 'You are on dark theme' : 'Your are on light theme'}</span>
-      <span className={`transform transition ease-in-out duration-200 inline-block transform rounded-full`}>
+      <span className={`inline transform rounded-full`}>
         {isDark ? (
           <IconBox icon='Moon' className='w-6 h-6 text-gray-300 hover:text-gray-200' title={text} />
         ) : (
           <IconBox icon='Sun' className='w-6 h-6 text-[gold] opacity-80 hover:opacity-100' title={text} />
         )}
       </span>
-    </div>
+    </button>
   )
 }
