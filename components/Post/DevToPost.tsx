e9d2720fca90a6a88da454f4cdcb3034e61f419c
azrizhaziq <azrizhaziq@gmail.com>
2021-07-20 11:22:12 +0800

    update time format

--- components/Post/DevToPost.tsx
+++ components/Post/DevToPost.tsx
@@ -2,11 +2,23 @@ import Link from 'next/link'
 import { Tag } from '../Tag'
 import { IconBox } from '../Icons'
 import { Devto_Post } from '@helpers'
+import format from 'date-fns/format'
 import formatDistance from 'date-fns/formatDistance'
 
-export function DevToPost({ post }: { post: Devto_Post }): JSX.Element {
-  const date = formatDistance(new Date(), new Date(post.published_timestamp))
+const DevToPostTimeStamp = ({ time }: { time: string }): JSX.Element => {
+  const date = formatDistance(new Date(), new Date(time))
+  const dateFormatted = format(new Date(time), 'dd/MM/yyyy')
 
+  return (
+    <time className='text-xs text-gray-400'>
+      <div className='sr-only'>{dateFormatted}</div>
+      <span className='block group-hover:hidden'>{date}</span>
+      <span className='hidden group-hover:block'>{dateFormatted}</span>
+    </time>
+  )
+}
+
+export function DevToPost({ post }: { post: Devto_Post }): JSX.Element {
   return (
     <article className='relative py-8 flex flex-col'>
       <a
@@ -14,7 +26,7 @@ export function DevToPost({ post }: { post: Devto_Post }): JSX.Element {
         onClick={e => e.stopPropagation()}
         target='_blank'
         rel='noopener noreferrer'
-        className='absolute left-[-30px] top-[53px]'>
+        className='absolute left-[-30px] top-[60px]'>
         <IconBox
           icon='Devto'
           title='Written at Devto'
@@ -23,10 +35,7 @@ export function DevToPost({ post }: { post: Devto_Post }): JSX.Element {
       </a>
       <Link href={'/'}>
         <a className={'space-y-2 text-gray-700 group dark:text-indigo-300 opacity-80 hover:opacity-100'}>
-          <time className='text-xs text-gray-400'>
-            <span className='block group-hover:hidden'>{date}</span>
-            <span className='hidden group-hover:block'>{new Date(post.published_timestamp).toLocaleDateString()}</span>
-          </time>
+          <DevToPostTimeStamp time={post.published_timestamp} />
           <h2 className='flex items-center text-2xl text-bold font-medium'>{post.title}</h2>
           <p className='hidden! md:block line-clamp-0 leading-relaxed'>{post.description}</p>
           <div className='flex flex-wrap space-x-1'>
