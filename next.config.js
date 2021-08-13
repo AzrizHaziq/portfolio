dee0e9e65487b1a9565cced15efbaf53f3ea5ae7
azrizhaziq <azrizhaziq@gmail.com>
2021-08-13 21:32:38 +0800

    node-canvas error try out solution from github issue

--- next.config.js
+++ next.config.js
@@ -1,3 +1,14 @@
+// https://github.com/Automattic/node-canvas/issues/1779#issuecomment-895885846
+// For building on vercel: https://github.com/Automattic/node-canvas/issues/1779
+if (
+  process.env.LD_LIBRARY_PATH == null ||
+  !process.env.LD_LIBRARY_PATH.includes(`${process.env.PWD}/node_modules/canvas/build/Release:`)
+) {
+  process.env.LD_LIBRARY_PATH = `${process.env.PWD}/node_modules/canvas/build/Release:${
+    process.env.LD_LIBRARY_PATH || ''
+  }`
+}
+
 const withPWA = require('next-pwa')
 const withPlugins = require('next-compose-plugins')
 const withBundleAnalyzer = require('@next/bundle-analyzer')({
