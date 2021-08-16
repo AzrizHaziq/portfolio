3c768c671e3cd88baf6e1af111c7db570e0a1a05
azrizhaziq <azrizhaziq@gmail.com>
2021-08-16 13:07:18 +0800

    update with custom primary color in ImgSkeleton

--- components/ImgSkeleton.tsx
+++ components/ImgSkeleton.tsx
@@ -3,12 +3,12 @@ const shimmer = (w: number, h: number) => `
 <svg width="${w}" height="${h}" xmlns="http://www.w3.org/2000/svg" xmlns:xlink="http://www.w3.org/1999/xlink">
   <defs>
     <linearGradient id="g">
-      <stop stop-color="#333" offset="20%" />
-      <stop stop-color="#222" offset="50%" />
-      <stop stop-color="#333" offset="70%" />
+      <stop stop-color="#E0E7FF" offset="20%" />
+      <stop stop-color="#EEF2FF" offset="50%" />
+      <stop stop-color="#E0E7FF" offset="70%" />
     </linearGradient>
   </defs>
-  <rect width="${w}" height="${h}" fill="#333" />
+  <rect width="${w}" height="${h}" fill="#E0E7FF" />
   <rect id="r" width="${w}" height="${h}" fill="url(#g)" />
   <animate xlink:href="#r" attributeName="x" from="-${w}" to="${w}" dur="1s" repeatCount="indefinite"  />
 </svg>`
