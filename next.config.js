f7b3c6c009ea1165aecad7e595c916a405af9754
azrizhaziq <azrizhaziq@gmail.com>
2021-09-05 15:49:20 +0800

    update security headers

--- next.config.js
+++ next.config.js
@@ -27,5 +27,51 @@ module.exports = withPlugins(
     images: {
       domains: ['github.com', 'opengraph.githubassets.com', 'vercel.app', 'github.io', 'source.unsplash.com'],
     },
+    async headers() {
+      return [
+        {
+          source: '/(.*)',
+          headers: securityHeaders,
+        },
+      ]
+    },
   },
 )
+
+// https://github.com/leerob/leerob.io/blob/main/next.config.js
+const securityHeaders = [
+  // https://securityheaders.com
+  // https://developer.mozilla.org/en-US/docs/Web/HTTP/CSP
+  {
+    key: 'Content-Security-Policy',
+    value: [
+      `default-src 'self'`,
+      `script-src 'self' 'unsafe-eval' 'unsafe-inline' *.youtube.com *.twitter.com cdn.usefathom.com *.google-analytics.com`,
+      `child-src *.youtube.com *.google.com *.twitter.com`,
+      `style-src 'self' 'unsafe-inline' *.googleapis.com`,
+      `img-src * blob: data:`,
+      `media-src 'none'`,
+      `connect-src *`,
+      `font-src 'self'`,
+    ].join('; '),
+  },
+
+  // https://developer.mozilla.org/en-US/docs/Web/HTTP/Headers/Referrer-Policy
+  { key: 'Referrer-Policy', value: 'origin-when-cross-origin' },
+
+  // https://developer.mozilla.org/en-US/docs/Web/HTTP/Headers/X-Frame-Options
+  { key: 'X-Frame-Options', value: 'DENY' },
+
+  // https://developer.mozilla.org/en-US/docs/Web/HTTP/Headers/X-Content-Type-Options
+  { key: 'X-Content-Type-Options', value: 'nosniff' },
+
+  // https://developer.mozilla.org/en-US/docs/Web/HTTP/Headers/X-DNS-Prefetch-Control
+  { key: 'X-DNS-Prefetch-Control', value: 'on' },
+
+  // https://developer.mozilla.org/en-US/docs/Web/HTTP/Headers/Strict-Transport-Security
+  { key: 'Strict-Transport-Security', value: 'max-age=31536000; includeSubDomains; preload' },
+
+  // https://developer.mozilla.org/en-US/docs/Web/HTTP/Headers/Feature-Policy
+  // Opt-out of Google FLoC: https://amifloced.org/
+  { key: 'Permissions-Policy', value: 'camera=(), microphone=(), geolocation=(), interest-cohort=()' },
+]
